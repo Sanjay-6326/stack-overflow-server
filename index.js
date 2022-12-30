69c8b672d23c160ae16b52baf9f828a64557d04a
Sanjay-6326 <121351017+Sanjay-6326@users.noreply.github.com>
2022-12-30 15:03:13 +0530

    Update index.js

--- index.js
+++ index.js
@@ -23,9 +23,9 @@ app.use('/answer', answerRoutes)
 
 const PORT = process.env.PORT || 5000
 
-mongoose.set("strictQuery", false);
+const DATABASE_URL = process.env.CONNECTION_URL
 
-mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true})
+mongoose.connect( DATABASE_URL, { useNewUrlParser: true, useUnifiedTopology: true})
     .then(() => app.listen(PORT, () => {console.log(`server running on port ${PORT}`)}))
     .catch((err) => console.log(err.message))
 
