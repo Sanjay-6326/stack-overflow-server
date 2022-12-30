141eb5b2d224589b95fe7c571e232d4607635247
Sanjay-6326 <sanjaybahukhandi100@gmail.com>
2022-12-31 00:23:31 +0530

    first commit

--- controllers/auth.js
+++ controllers/auth.js
@@ -16,7 +16,7 @@ export const signup = async (req, res) => {
         const token = jwt.sign({ email: newUser.email, id: newUser._id}, process.env.JWT_SECRET, { expiresIn: '1h'});
         res.status(200).json({ result: newUser, token })
     } catch (error) {
-        res.status(500).json("Somethin went wrong...")
+        res.status(500).json("Something went wrong...")
     }
 } 
 
